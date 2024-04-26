35372efc97c7f3464ff8dfe913a7bc4e0c511215
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2024-04-26 09:40:16 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,11 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findById ===");
@@ -38,14 +41,29 @@ public class Program {
 		System.out.println("Inserted! New id = " + insertSeller.getId());
 		*/
 		
+		/*
 		System.out.println("\n=== TEST 5: seller update ===");
-		seller = sellerDao.findById(10);
-		seller.setName("Pedro");
-		seller.setEmail("pedro@gmail.com");
-		seller.setBaseSalary(3500.0);
-		seller.setDepartment(new Department(2, "Electronics"));
+		seller = sellerDao.findById(12);
+		seller.setName("Esther");
+		seller.setEmail("esther@gmail.com");
+		seller.setBaseSalary(6530.99);
+		seller.setDepartment(new Department(4, "Books"));
 		sellerDao.update(seller);
 		System.out.println("Updated completed! Seller updated to: " + seller);
+		*/
 		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.print("Type seller id for deletion: ");
+		int id = sc.nextInt();
+		seller = sellerDao.findById(id);
+		if(seller == null) {
+			System.out.println("Deletion failed! Id does not exist.");
+		} else {
+			sellerDao.deleteById(id);
+			System.out.println("Deletion complete! Deleted seller: " + seller);
+		}
+		
+		
+		sc.close();
 	}
 }
